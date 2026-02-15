4ebd686c743dd3b5366f10c406fee44f0c69223a
satzsaravanan <ajoashok7@gmail.com>
2026-02-15 14:28:21 +0530

    i completed Evening

--- src/test/java/GITTask.java
+++ src/test/java/GITTask.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 	
 
 	System.out.println("Hi Updated code repo");
-	System.out.println("Am done the code changes in afternoon");
+	System.out.println("Am done the code changes in evening");
 
 }
 
