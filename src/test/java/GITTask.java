8909fd2ef48b6962f440f52458c27b2214fe99a4
satzsaravanan <ajoashok7@gmail.com>
2026-02-15 13:37:01 +0530

    Code changes aftnun

--- src/test/java/GITTask.java
+++ src/test/java/GITTask.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GITTask {
 public static void main(String[] args) {
-	WebDriver fbook = new ChromeDriver();
-	fbook.get("https://www.facebook.com/");
+	System.out.println("Hi Updated code repo");
 	
-	fbook.manage().window().maximize();
 
 }
 }
